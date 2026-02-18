39f3d8764c01000e1ba9694b20ac04fe98eaa1f9
Shreya07YC <shreya.chouhan@yunicorn.in>
2026-02-18 11:09:13 +0530

    fix the bug of login register

--- config/cors.ts
+++ config/cors.ts
@@ -8,7 +8,7 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: ['http://localhost:5173'],
+  origin: true,
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
   exposeHeaders: [],
