75e914d3395eafd3788569612082bcd86c1c580e
shubh-2209 <shubh.porwal@yunicorn.in>
2026-02-19 14:59:10 +0530

    fix: live streaming issue

--- app/services/live_stream_service.ts
+++ app/services/live_stream_service.ts
@@ -42,7 +42,6 @@ export default class LiveStreamService {
   static configure() {
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
     })
--- bin/server.ts
+++ bin/server.ts
@@ -34,6 +34,9 @@ new Ignitor(APP_ROOT, { importer: IMPORTER })
     app.booting(async () => {
       await import('#start/env')
     })
+    app.starting(async () => {
+      await import('#start/socket')
+    })
     app.listen('SIGTERM', () => app.terminate())
     app.listenIf(app.managedByPm2, 'SIGINT', () => app.terminate())
   })
